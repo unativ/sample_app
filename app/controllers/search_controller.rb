5fb55fbbb4f53a5280b4596e20dc8acea7aa3e42
unativ <uri.nativ@gmail.com>
2012-10-29 08:27:43 +0200

    Nicer Search page.
    Updated Search Query to LIKE

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,28 @@ class SearchController < ApplicationController
   def search
     #@results = Micropost.where('match (content) against (?)', params[:query])
     #@results = Micropost.where("match (content) against ('#{params[:query]}')")
-    @results = Micropost.find_by_sql("select * from microposts where match (content) against ('#{params[:query]}')")
+    #@results = Micropost.find_by_sql("select * from microposts where match (content) against ('#{params[:query]}')")
+
+
+
+
+    # SQL injection
+    @results = Micropost.where("content LIKE '%#{params[:query]}%'").all
+    
+    # good (but still % injection)
+    #@results = Micropost.where('content LIKE ?', "%#{params[:query]}")
+   
+    #unless params[:token].nil?
+      #User.where(:email => params[:email], :remember_token => params[:token]).all
+    #end 
+    
+    t = []
+    t.push(nil)
+
+    #@results = Micropost.where(:content => params[:query]).all
 
     # SQL string: Check' )) or 1 = 1  -- 
+    # ') union select name, email, remember_token, 4, 5 from users -- 
+    # ') union select name, remember_token, 3, 4, 5 from users -- 
   end
 end
